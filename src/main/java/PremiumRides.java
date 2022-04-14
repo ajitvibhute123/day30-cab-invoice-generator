7227a679d9b3c2e20ddcba2fbbfae38f15cea700
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-15 02:27:33 +0530

    Added with changes in file

--- src/main/java/PremiumRides.java
+++ src/main/java/PremiumRides.java
@@ -1,4 +1,15 @@
-
 public class PremiumRides {
+    public static PremiumRides Normal;
 
+    private final double costperkm;
+    private final double costpermin;
+
+    PremiumRides(double costperkm, double costpermin, double minfare){
+        this.costperkm=costperkm;
+        this.costpermin=costpermin;
+    }
+
+    public double calcCostOfCabRide(Rides ride) {
+        return ride.distance * costperkm + ride.time * costpermin;
+    }
 }
